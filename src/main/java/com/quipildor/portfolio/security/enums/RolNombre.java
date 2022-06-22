1d3f49f88ac31ff0e567f0bb4df9688345b9f835
kipildor <leandro_1980@live.com.ar>
2022-06-22 20:48:29 -0300

    Probando JWT - V1

---
+++ src/main/java/com/quipildor/portfolio/security/enums/RolNombre.java
@@ -0,0 +1,6 @@
+package com.quipildor.portfolio.security.enums;
+
+public enum RolNombre {
+    //El aplicativo tiene dos roles, admin(Tiene todos los permisos CRUD) y user(ve las torres)
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/quipildor/portfolio/security/util/CreateRoles.java
@@ -0,0 +1,25 @@
+package com.quipildor.portfolio.security.util;
+/*
+import com.quipildor.portfolio.security.entity.Rol;
+import com.quipildor.portfolio.security.enums.RolNombre;
+import com.quipildor.portfolio.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateRoles implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
+    }
+
+} 
+*/
