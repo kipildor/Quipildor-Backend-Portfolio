76269fdbe345acaedf00ef6fe55fd1ae2985ec7a
kipildor <leandro_1980@live.com.ar>
2022-07-08 21:26:38 -0300

    Alta de Exp. Laboral con Logo, en la tabla imagen

---
+++ src/main/java/com/quipildor/portfolio/cloudinary/repository/ImagenRepository.java
@@ -0,0 +1,12 @@
+package com.quipildor.portfolio.cloudinary.repository;
+
+import com.quipildor.portfolio.cloudinary.entity.Imagen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ImagenRepository extends JpaRepository<Imagen, Integer> {
+    List<Imagen> findByOrderById();
+}
